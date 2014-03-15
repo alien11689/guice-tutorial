ff39d6ecd76e66ea443325171a5f8fc3b7bbd75f
Dominik Adam Przybysz <alien11689@gmail.com>
2014-03-16 00:20:10 +0100

    added some simple tests

--- 01-the-simplest-injection/src/test/java/com/blogspot/przybyszd/guicetutorial/simplestinjection/PersonControllerIT.java
+++ 01-the-simplest-injection/src/test/java/com/blogspot/przybyszd/guicetutorial/simplestinjection/PersonControllerIT.java
@@ -1,7 +1,10 @@
 package com.blogspot.przybyszd.guicetutorial.simplestinjection;
+
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
 
 /**
  * Created by alien on 15.03.14.
@@ -9,10 +12,33 @@ import org.junit.Test;
 public class PersonControllerIT {
 
     @Test
-    public void shouldGetPersonControllerFromInjectorAndPersistPerson(){
+    public void shouldGetPersonValidatorWithoutException() {
+        //given
         Injector injector = Guice.createInjector();
-        PersonController userController = injector.getInstance(PersonController.class);
+        //when
+        PersonValidator personValidator = injector.getInstance(PersonValidator.class);
+        //then
+        assertNotNull(personValidator);
+    }
+
+    @Test
+    public void shouldGetPersonDaoWithoutException() {
+        //given
+        Injector injector = Guice.createInjector();
+        //when
+        PersonDao personDao = injector.getInstance(PersonDao.class);
+        //then
+        assertNotNull(personDao);
+    }
+
+    @Test
+    public void shouldGetPersonControllerFromInjectorAndPersistPerson() {
+        //given
+        Injector injector = Guice.createInjector();
         Person user = new Person("John", "Smith", 25);
-        userController.add(user);
+        //when
+        PersonController userController = injector.getInstance(PersonController.class);
+        //then
+        userController.add(user); // call without NPE
     }
 }
