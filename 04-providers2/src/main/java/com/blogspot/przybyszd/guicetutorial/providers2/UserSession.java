1dcfafb243d95b04430965996b03c7edea16a5a4
Dominik Adam Przybysz <alien11689@gmail.com>
2014-04-05 13:15:39 +0200

    added user session implementation

--- 04-providers2/src/main/java/com/blogspot/przybyszd/guicetutorial/providers2/UserSession.java
+++ 04-providers2/src/main/java/com/blogspot/przybyszd/guicetutorial/providers2/UserSession.java
@@ -3,5 +3,4 @@ package com.blogspot.przybyszd.guicetutorial.providers2;
 /**
  * Created by alien on 05.04.14.
  */
-public class UserSession {
-}
+public class UserSession {}
--- 04-providers2/src/test/java/com/blogspot/przybyszd/guicetutorial/providers2/SequenceGeneratorTest.java
+++ 04-providers2/src/test/java/com/blogspot/przybyszd/guicetutorial/providers2/SequenceGeneratorTest.java
@@ -2,6 +2,7 @@ package com.blogspot.przybyszd.guicetutorial.providers2;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Provider;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -30,6 +31,18 @@ public class SequenceGeneratorTest {
         //when
         SequenceGenerator sequenceGenerator1 = injector.getInstance(SequenceGenerator.class);
         SequenceGenerator sequenceGenerator2 = injector.getInstance(SequenceGenerator.class);
+        //then
+        assertEquals(sequenceGenerator1, sequenceGenerator2);
+    }
+
+    @Test
+    public void shouldGetTheSameSequenceGeneratorEachTimeFromProvider(){
+        //given
+        Injector injector = Guice.createInjector();
+        Provider<SequenceGenerator> sut = injector.getProvider(SequenceGenerator.class);
+        //when
+        SequenceGenerator sequenceGenerator1 = sut.get();
+        SequenceGenerator sequenceGenerator2 = sut.get();
         //then
         assertEquals(sequenceGenerator1, sequenceGenerator2);
     }
