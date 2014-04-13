fe2244e002219856e47ac543a7d75014b084ca50
Dominik Adam Przybysz <alien11689@gmail.com>
2014-04-13 20:02:44 +0200

    make test pass

--- 05-interfaces-default-implementations/src/main/java/com/blogspot/przybyszd/guicetutorial/interfaces/ReportWriter.java
+++ 05-interfaces-default-implementations/src/main/java/com/blogspot/przybyszd/guicetutorial/interfaces/ReportWriter.java
@@ -1,8 +1,11 @@
 package com.blogspot.przybyszd.guicetutorial.interfaces;
 
+import com.google.inject.ImplementedBy;
+
 /**
  * Created by alien on 13.04.14.
  */
+@ImplementedBy(PdfReportWriter.class)
 public interface ReportWriter {
     void write();
 }
