e7766e9c16e6b59a4322d7aa23368f821c5bae92
Dominik Adam Przybysz <alien11689@gmail.com>
2014-04-27 12:27:01 +0200

    Create module

---
+++ 06-injecting-injector/src/test/java/com/blogspot/przybyszd/guicetutorial/injectinginjector/InjectingInjectorTest.java
@@ -0,0 +1,29 @@
+package com.blogspot.przybyszd.guicetutorial.injectinginjector;
+
+import org.junit.Test;
+
+/**
+ * Created by alien on 27.04.14.
+ */
+public class InjectingInjectorTest {
+
+    @Test
+    public void shouldInjectInjectorFromInjector(){
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    public void shouldInjectInjectorToClass(){
+        //given
+
+        //when
+
+        //then
+
+    }
+}
