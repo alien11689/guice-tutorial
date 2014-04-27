8d9cffea454f89284118a46ee5c9248fc126b548
Dominik Adam Przybysz <alien11689@gmail.com>
2014-04-27 13:07:48 +0200

    Add getting builder from factory

---
+++ 06-injecting-injector/src/main/java/com/blogspot/przybyszd/guicetutorial/injectinginjector/builders/DefaultBuilder.java
@@ -0,0 +1,8 @@
+package com.blogspot.przybyszd.guicetutorial.injectinginjector.builders;
+
+import com.blogspot.przybyszd.guicetutorial.injectinginjector.Builder;
+
+/**
+ * Created by alien on 27.04.14.
+ */
+public class DefaultBuilder implements Builder{}
--- 06-injecting-injector/src/test/java/com/blogspot/przybyszd/guicetutorial/injectinginjector/InjectingInjectorTest.java
+++ 06-injecting-injector/src/test/java/com/blogspot/przybyszd/guicetutorial/injectinginjector/InjectingInjectorTest.java
@@ -1,5 +1,8 @@
 package com.blogspot.przybyszd.guicetutorial.injectinginjector;
 
+import com.blogspot.przybyszd.guicetutorial.injectinginjector.builders.DefaultBuilder;
+import com.blogspot.przybyszd.guicetutorial.injectinginjector.builders.FirstBuilder;
+import com.blogspot.przybyszd.guicetutorial.injectinginjector.builders.SecondBuilder;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.junit.Test;
@@ -23,12 +26,35 @@ public class InjectingInjectorTest {
     }
 
     @Test
-    public void shouldInjectInjectorToClass(){
+    public void shouldGetFirstBuilderFromFactory(){
         //given
         Injector sut = Guice.createInjector();
-        //when
         BuilderFactory builderFactory = sut.getInstance(BuilderFactory.class);
+        //when
+        Builder builder = builderFactory.getBuilder("first");
         //then
-        assertTrue(builderFactory.getInjector() instanceof Injector);
+        assertTrue(builder instanceof FirstBuilder);
+    }
+
+    @Test
+    public void shouldGetSecondBuilderFromFactory(){
+        //given
+        Injector sut = Guice.createInjector();
+        BuilderFactory builderFactory = sut.getInstance(BuilderFactory.class);
+        //when
+        Builder builder = builderFactory.getBuilder("second");
+        //then
+        assertTrue(builder instanceof SecondBuilder);
+    }
+
+    @Test
+    public void shouldGetDefaultBuilderFromFactory(){
+        //given
+        Injector sut = Guice.createInjector();
+        BuilderFactory builderFactory = sut.getInstance(BuilderFactory.class);
+        //when
+        Builder builder = builderFactory.getBuilder("unknown");
+        //then
+        assertTrue(builder instanceof DefaultBuilder);
     }
 }
