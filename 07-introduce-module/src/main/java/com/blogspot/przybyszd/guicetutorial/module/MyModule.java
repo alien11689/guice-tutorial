2cfc5ce51d1b7497ddb0022de0e8ae74c2b33546
Dominik Adam Przybysz <alien11689@gmail.com>
2014-05-01 21:31:38 +0200

    Make test with singleton pass

--- 07-introduce-module/src/main/java/com/blogspot/przybyszd/guicetutorial/module/MyModule.java
+++ 07-introduce-module/src/main/java/com/blogspot/przybyszd/guicetutorial/module/MyModule.java
@@ -1,6 +1,7 @@
 package com.blogspot.przybyszd.guicetutorial.module;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
 
 public class MyModule extends AbstractModule{
     @Override
@@ -14,5 +15,10 @@ public class MyModule extends AbstractModule{
         bind(ThirdClass.class).toProvider(ThirdClassProvider.class);
         bind(FourthClass.class).toInstance(new FourthClass(new Dependency()));
         bind(SomeClass.class).to(FifthClass.class);
+        try {
+            bind(SixthClass.class).toConstructor(SixthClass.class.getConstructor(Dependency.class)).in(Singleton.class);
+        } catch(NoSuchMethodException e) {
+            e.printStackTrace();
+        }
     }
 }
