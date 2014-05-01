b557c65460c9e24974a3874ddaa4ebf71836d5fa
Dominik Adam Przybysz <alien11689@gmail.com>
2014-05-01 21:23:22 +0200

    Make three tests pass

--- 07-introduce-module/src/main/java/com/blogspot/przybyszd/guicetutorial/module/MyModule.java
+++ 07-introduce-module/src/main/java/com/blogspot/przybyszd/guicetutorial/module/MyModule.java
@@ -5,6 +5,11 @@ import com.google.inject.AbstractModule;
 public class MyModule extends AbstractModule{
     @Override
     protected void configure() {
-        
+        try {
+            bind(FirstClass.class).toConstructor(FirstClass.class.getConstructor(Dependency.class));
+        } catch(NoSuchMethodException e) {
+            // But i know it exists
+            e.printStackTrace();
+        }
     }
 }
